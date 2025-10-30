b0b34ef8b53305b455508ea582c869cdea88a5b0
mafenandaup <mafenandaup@gmail.com>
2025-10-30 08:48:23 -0300

    feat: login page

---
+++ arielle-front/src/pages/LoginPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../index.css'
+
+const LoginPage = () => {
+  return (
+    <>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-3xl text-amber-800 font-segoe-ui-bold">Testando</h1>
+    </div>
+    </>
+  )
+}
+
+export default LoginPage
